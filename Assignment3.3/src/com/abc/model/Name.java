5cf95e6eb59293ab2495eead969530198416791b
Tim Oien <oien.tim@gmail.com>
2014-11-24 01:02:29 -0600

    name.java added

---
+++ Assignment3.3/src/com/abc/model/Name.java
@@ -0,0 +1,41 @@
+package com.abc.model;
+
+public class Name {
+
+	private int userid;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	
+	public int getuserid() {
+		return userid;
+	}
+	public void setuserid(int userid) {
+		this.userid = userid;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	@Override
+	public String toString() {
+		return "User [userid=" + userid + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", username=" + userName + "]";
+	}
+	
+	
+}
